f01a30a209c737215f0ddcfaf758c869a5758063
emillyvieira <emilly.vieira190505@gmail.com>
2025-03-13 22:13:52 -0300

    fazer menu inicial

--- app/page.js
+++ app/page.js
@@ -1,66 +1,66 @@
-"use client";
+'use client'
 
-import { use, useState, useEffect } from "react";
+import Footer from "@/components/Footer";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [chat, setChat] = useState([]);
-  const [message, setMessage] = useState("Teste");
 
-  useEffect(()=> {
+  const [chat, setChat] = useState([])
+  const [message, setMessage] = useState('ola')
+
+  useEffect(() => {
     const getMessages = async () => {
-      const response = await fetch("http://localhost:3000/messages");
-      const data = await response.json();
-      setChat(data);
+      const response = await fetch('http://localhost:3000/messages')
+      const data = await response.json()
+      setChat(data)
     }
-    getMessages();
-  }
-  ,[])
-   
-  const handleSubmit = async () => {
-    setChat([...chat, { message }]);
-    const response = await fetch("http://localhost:3000/ai", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ question: message }),
-    });
+    getMessages()
+  },[])
 
-    const data = await response.json();
-    setChat([...chat, { message }, { message: data.answer }]);
-  };
+  useEffect(() => {
+    console.log(message)
+  },[message])
+
+  const handleSubmit = async () => {
+    const response = await fetch('http://localhost:3000/ia', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({question: message})
+    })
+
+    const data = await response.json()
+    setChat([...chat, {message: message}, {message: data.response}])
+    setMessage('')
+  }
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center gap-6 pb-[150px] px-28 font-[family-name:var(--font-geist-sans)]">
+      
       {chat.map((item, index) => (
-        <div key={index} className="bg-zinc-700 p-2 rounded-lg shadow-lg">
+        <div key={index} className="bg-zinc-700 p-4 rounded-lg text-white text-center">
           {item.message}
         </div>
       ))}
-
-      <div className="absolute position:absolute bottom-0 w-full mx-auto bg-zinc-700 p-4 rounded-lg shadow-lg">
-        <textarea
-          className="w-full h-20 p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-500 resize-none"
-          placeholder="Digite sua mensagem aqui..."
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button
-          className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition duration-300 ease-in-out transform hover:-translate-y-1"
-          onClick={handleSubmit}
-          style={styles.button}
-
-        >
-          Enviar
-        </button>
+      
+      <div className="fixed bottom-6 mx-auto bg-zinc-700 flex items-center justify-center p-4 rounded-lg text-white text-center gap-2">
+        <textarea value={message} onChange={(e) => setMessage(e.target.value)} className="w-[600px] border border-zinc-900 rounded p-2 resize-none"></textarea>
+        <button style={styles.button} onClick={handleSubmit}>Enviar</button>
       </div>
+
+      <Footer />
     </div>
   );
 }
 
-
 const styles = {
-    button: {
-    
-      cursor: "pointer",
-    },
+  button: {
+    backgroundColor: '#456456',
+    border: '1px solid #CCC',
+    borderRadius: '5px',
+    color: '#FFF',
+    padding: '5px 10px',
+    cursor: 'pointer'
+  }
 }
